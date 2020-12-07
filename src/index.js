75e57fef67d5b1f23d6b6ae183817211a2c4f6e6
Visvaldas <visva.rapalis@gmail.com>
2020-12-07 14:04:43 +0200

    Get data from openweathermap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './style.css';
 
-console.log('Helloooooo')
+window.addEventListener('load', () => {
+  let lon;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lon = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://"
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API}`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    });
+  } else {
+    h1.textContent = 'Please anable location sharing to see a temperature.';
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -14,4 +15,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv()
+  ]
 };
