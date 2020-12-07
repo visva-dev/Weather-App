84e5b1f37c16b3a5572e4e78defcd1b9585e60ea
Visvaldas <visva.rapalis@gmail.com>
2020-12-07 15:42:07 +0200

    Update info in the browser with data from api

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import './style.css';
 window.addEventListener('load', () => {
   let lon;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-dsc');
+  let temperatureDegree = document.querySelector('.degree');
+  let temperatureTimeZone = document.querySelector('.location-timezone');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       lon = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = "https://"
+      const proxy = 'https://';
       const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API}`;
 
       fetch(api)
@@ -18,6 +21,10 @@ window.addEventListener('load', () => {
         })
         .then((data) => {
           console.log(data);
+          const { temp } = data.main;
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = data.weather[0].description;
+          temperatureTimeZone.textContent = data.sys.country;
         });
     });
   } else {
